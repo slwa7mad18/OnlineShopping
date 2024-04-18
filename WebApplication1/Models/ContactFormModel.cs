f4d719fd6715b90c2397dac8667e83bf04932ee3
Salma Ahmed <salma2001ahmednady@gmail.com>
2024-04-18 21:35:14 +0200

    ContactPage

---
+++ WebApplication1/Models/ContactFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Models
+{
+    public class ContactFormModel
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        public string Message { get; set; }
+
+
+    }
+}
