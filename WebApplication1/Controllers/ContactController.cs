1480f1309ce9b9f1a2c0019d7b2ab4cf87f3d8d1
Salma Ahmed <salma2001ahmednady@gmail.com>
2024-04-18 21:35:14 +0200

    AboutUS PageContactPage

---
+++ WebApplication1/Controllers/ContactController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopping.Models;
+using WebApplication1.Models;
+
+namespace OnlineShopping.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly Context _context;
+
+        public ContactController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitContactForm(ContactFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // _context.ContactForms.Add(model);
+                //_context.SaveChanges();
+                return RedirectToAction("ThankYou");
+            }
+
+            return View("Index", model);
+        }
+
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
